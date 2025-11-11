60e61b964dc17bc5684e53a4b69afa516a16aa69
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-12 00:03:35 +0530

    [MODIFEIED]: Jayesh Python code for using the Arithmatic operators to perform the simple ADD,SUB, MULT,Div in python

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+a,b = 10,20
+sum = a + b
+
+print("Addition = ",sum)
+print("subtraction = ",a-b)
+print("Multiplication = ",a*b)
+print("Division = ",a/b)
+print("Reminder = ",a%b)
+print("a^b = ",a**b)
