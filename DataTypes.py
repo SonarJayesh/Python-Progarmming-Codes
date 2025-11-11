b2f4c29bf3f984841229876f578452955ad32e34
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-12 00:05:47 +0530

    [MODIFEIED]: Jayesh Python code for Data Types in java and print values od that data

---
+++ DataTypes.py
@@ -0,0 +1,19 @@
+rollNo=63
+
+name = 'Jayesh Sonar'
+name2 = "Jayesh Soanr"
+name3 = '"Jayesh Sonar"'
+
+salary = 23200.53
+
+isSalary = True
+
+a = None
+
+print("Type of Roll No = ",type(rollNo))
+print("Type of name = ",type(name))
+print("Type of name2 = ",type(name2))
+print("Type of name3 = ",type(name3))
+print("Type of salary = ",type(salary))
+print("Type of isSalary = ",type(isSalary))
+print("Type of a = ",type(a))
