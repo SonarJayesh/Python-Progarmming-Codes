d16ec546010eb38a8d43cb43d81108d7cd947c57
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-13 18:56:43 +0530

    [MODIFEIED]: Jayesh Python code for Practice_Question1 for input the 2 numbers and perform some addition in python

---
+++ Practice_Question1.py
@@ -0,0 +1,6 @@
+# Q1  Write a program to input 2 Number & print their sum
+
+first = int(input("Enter the First Number :"))
+second = int(input("Enter the Second Number :"))
+
+print("Sum = ",first + second)
