2176f9c7af26e780f38a9515f9a3f7189ed82018
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-14 18:42:22 +0530

    [MODIFEIED]: Jayesh Python code for [200~input 2 int Numbers, a and b.

---
+++ Practice_Question4.py
@@ -0,0 +1,7 @@
+# Q4. Write a program to input 2 int Numbers, a and b.
+#     Print true if a is Greater than or equal to b. If not print False
+ 
+a = int(input("Enter the int a = "))
+b = int(input("Enter the int b = "))
+
+print(a>=b )
