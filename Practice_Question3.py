30768faff96735e6febc62c432e19993c44ca46e
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-14 18:41:18 +0530

    [MODIFEIED]: Jayesh Python code for Practice_Question3 input 2 Floating number & print their Average.

---
+++ Practice_Question3.py
@@ -0,0 +1,7 @@
+# Q3. Write a program to input 2 Floating number & print their Average.
+
+num1 = float(input("Enter the float num1 = "))
+num2 = float(input("Enter the float num2 = "))
+
+avg = (num1+num2)/2
+print("Average = ",avg)
