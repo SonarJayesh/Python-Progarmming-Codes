1e669d820134aa5279f69a2cab7cd96ac2d2913f
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-17 00:19:38 +0530

    [ADDED]: Jayesh Python Code for input the student marks and given the grade related to marks using if_elseif conditional statements in python

---
+++ Grade_student_based_on_marks.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter the marks: "))
+
+if(marks >= 90):
+    print("Grade = A")
+elif(marks>90 and marks >= 80):
+    print("Grade = B")        
+elif(marks >= 70 and marks >= 70):
+    print("Grade = C")
+elif(marks >70 and marks>=35):
+    print("Grade = D")
+else:
+    print("Student is Fail")    
+
