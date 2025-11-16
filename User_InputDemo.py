9c8d04aef7f75321db276f87191d2ac7eb753ab8
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-16 15:22:07 +0530

    [ADDED]: Jayesh Python Code for input the user information and print the information by user given input

---
+++ User_InputDemo.py
@@ -0,0 +1,14 @@
+a = input("Enter your Name: ")
+print(a)
+
+val =int(input("Enter the Value:"))
+print(type(val), val) 
+
+
+name = input("Enter the Name: ")
+age = int(input("Enter the Age: "))
+marks = float(input("Enter the Marks: "))
+
+print("welcome",name)
+print("age = ",age)
+print("Marks = ",marks)
