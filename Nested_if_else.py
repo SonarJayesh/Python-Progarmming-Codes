b1e1c2b9fd92de13a6ac24cc53d413bec189a03a
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-19 00:54:48 +0530

    [ADDED]: Jayesh Python Code Nested if_else code for Driverr can eligible for diving or not check using their Age

---
+++ Nested_if_else.py
@@ -0,0 +1,9 @@
+age  = 85
+
+if(age>=18):
+    if(age>=80):
+        print("Cannot Drive.")
+    else:
+        print("Can Drive.")    
+else:
+    print("Cannot Drive the Car Beacuse age is less than 18")
