4dd99afbc825ab5e43f2e8ad8481e415ff1547e1
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-19 00:56:08 +0530

    [ADDED]: Jayesh Python Code if_elif_else for trafic signal allocation

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+signal = "green"
+
+if(signal=="red"):
+    print("Stop the Car") #indentation
+elif(signal == "green"):
+    print("Run the Car") 
+elif(signal=="yellow"):
+    print("look")    
+else:
+    print("Light is Broken")
+    
+           
