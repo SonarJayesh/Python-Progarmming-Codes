8de9c4169ab85dc852673ed87581625b1f6b96b2
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-20 00:10:25 +0530

    [ADDED]: Jayesh Python progran to check if a number entered by the user is odd or even.

---
+++ Practice_Question7.py
@@ -0,0 +1,7 @@
+# write a progran to check if a number entered by the user is odd or even.
+num = int(input("Enter the Number:"))
+
+if(num %2==0):
+    print (num,"is Even")
+else:
+    print(num,"is Odd")       
