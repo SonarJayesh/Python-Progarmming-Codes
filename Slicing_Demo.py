0f9b136f0642d48f5bf9e63e48c4a5de6c55bae6
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-23 19:20:22 +0530

    [ADDED]: Jayesh Python progran to String Demo that code is perform many operations on slicing the String

---
+++ Slicing_Demo.py
@@ -0,0 +1,19 @@
+str1 = "Jayesh Sonar at Met's Institute of Management Bhujbal Knowledge city Nashik"
+print(str1+"\n")
+
+#Accessing the string using starting and ending index
+#Positive indexing
+print(str1[5:21])
+print(str1[:21])
+print(str1[5:])
+print(str1[:])
+print(str1[5:len(str1)])
+
+#Accessing the string using starting and ending index
+#Negative indexing
+
+str2 = "Apple is Red"
+
+print(str2+"\n")
+
+print(str2[-9:-3])
