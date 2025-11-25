6f004242705a577e089f993027de4823cf2e25cd
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-25 22:38:40 +0530

    [ADDED]: Jayesh Python Program for List Demo its creat a list and print and perform some operations on list like print update find index of list etc

---
+++ ListDemo.py
@@ -0,0 +1,11 @@
+marks = [94.4, 52.47, 89.50, 75.26, 90.55]
+print(marks)
+print(type(marks))
+print(marks[3])
+
+marks[2] = 70.55 # list is mutable
+print(marks)
+
+student = ["Jayesh", 94.63, "Nashik"]
+print(student)
+print(student[2])
