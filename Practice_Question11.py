4838bddec98793a83995a8c8058c87e2031c2f48
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-26 18:30:31 +0530

    [ADDED]: Jayesh Python program to check if a list contains a palindrom of elements.

---
+++ Practice_Question11.py
@@ -0,0 +1,10 @@
+#Write a program to check if a list contains a palindrom of elements.
+
+list = [1,2,3,2,1]
+list.copy
+rev = list
+
+if(list == rev):
+    print("Number is Palindrome")
+else:
+    print("Number is not Palindrome")
