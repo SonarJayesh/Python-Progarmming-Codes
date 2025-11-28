d043c0745a7f552375008aef9b4304a898721ebd
SonarJayesh <jayeshsonar2824@gmail.com>
2025-11-29 00:47:23 +0530

    [ADDED]: Jayesh Python program Figure out a way to store 9 & 9.0 as separate values in the set.

---
+++ Practice_Question17.py
@@ -0,0 +1,9 @@
+#Figure out a way to store 9 & 9.0 as separate values in the set.
+#(You can take help of built-in data types)
+
+value = {
+    ("int", 9),
+    ("float", 9.0)    
+    
+}
+print(value)
