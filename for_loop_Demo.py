12d303bacc9d7d1c1ff167f82db8e1ab08cfe16d
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-01 00:09:52 +0530

    [ADDED]: Jayesh Python Program for_loop demo for iterating the tuple or List and print it

---
+++ for_loop_Demo.py
@@ -0,0 +1,17 @@
+
+list = [10,20,30,40,50]
+vegitables = ["Potato","Brinjal", "Ladyfingure"]
+tuple = (10,5,6,20,18)
+str = "Jayesh Sonar"
+
+for val in list:
+    print(val)
+
+for veg in vegitables:
+    print(veg)    
+
+for tpl in tuple:
+    print(tpl)    
+
+for char  in str:
+    print(char)    
