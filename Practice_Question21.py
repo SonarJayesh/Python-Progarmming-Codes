b4d6eaf730bf914aca51ad91b5a7651d16d9568e
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-03 21:58:57 +0530

    [ADDED]: Jayesh Python program #Print the elements of the following list #\(L=[1,4,9,16,25,36,49,64,81,100]\).

---
+++ Practice_Question21.py
@@ -0,0 +1,11 @@
+#Print the elements of the following list
+#\(L=[1,4,9,16,25,36,49,64,81,100]\).
+
+list1 = [1,4,9,16,25,36,49,64,81,100]
+
+length = len(list1)-1
+print("Length of List = ",length)
+i = 0
+while i<=length:
+    print(list1[i])
+    i +=1
