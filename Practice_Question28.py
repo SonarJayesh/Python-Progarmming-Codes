17643b71ce50307cf8f49a184ec0bcfe56d11715
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-06 00:15:20 +0530

    [ADDED]: Jayesh Python program to find the sum of first n natural numbers.(using while)

---
+++ Practice_Question28.py
@@ -0,0 +1,10 @@
+#Write a program to find the sum of first n natural numbers. (using while)
+
+n =5
+sum=0
+i = 1
+
+while i<= n:
+    sum +=i
+    i +=1
+print("Total sum of first n natural number is : ",sum)    
