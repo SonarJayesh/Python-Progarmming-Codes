280e79d4aac6150ee75e8f4f36b81bc0066eed76
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-08 23:34:22 +0530

    [ADDED]: Jayesh Python Program for Perform the sun of given Values using Function

---
+++ Function_sum_Demo.py
@@ -0,0 +1,17 @@
+
+#Function Definition 
+def calc_sum(a,b): #Function Parameters
+    sum = a+b
+    print("Sum od A and B: ",sum)
+    return sum
+
+calc_sum(10,20) #Function Calling, Arguments
+calc_sum(5,9)
+calc_sum(22.2,30)
+
+#Space compaxity
+def calSum(x,y):
+    return x+y
+
+sum = calSum(10,12.5)
+print(sum)
