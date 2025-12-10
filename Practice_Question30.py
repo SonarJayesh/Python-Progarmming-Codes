deb2509cc771066681d3d0b34e000e37d97c4d3a
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-10 23:29:37 +0530

    [ADDED]: Jayesh Python for Write a function to print the lengh of a list.(list is the parameter)

---
+++ Practice_Question30.py
@@ -0,0 +1,10 @@
+#Write a function to print the lengh of a list.(list is the parameter)
+Names = ["Jayesh","Shubham", "Mahesh","Ritesh","Ishwar"]
+age = [21,18,23,15]
+
+def list_Demo(list):
+    Length=len(list)
+    print(Length)
+
+list_Demo(Names)
+list_Demo(age)    
