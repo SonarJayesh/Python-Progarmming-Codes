acaf3aca26f21049fc19e0273b2ee81253147f35
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-11 22:04:32 +0530

    [ADDED]: Jayesh Python for Write a function to input the number, if no is odd print Odd otherwise print Even

---
+++ Practice_Question34.py
@@ -0,0 +1,10 @@
+#Home work Question Write a function to input the number, if no is odd print Odd otherwise print Even
+
+def even_Odd():
+    num = int(input("Enter the Any Number"))
+    if num%2 !=0:
+        print(" No is ODD")
+    else:
+        print("No is EVEN")   
+
+even_Odd()
