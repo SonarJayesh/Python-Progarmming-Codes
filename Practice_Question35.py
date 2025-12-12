99d3a0fbcd7eb036570d376a1796ffca07f16384
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-12 23:17:12 +0530

    [ADDED]: Jayesh Python for Write a recursive function to calculate the sum of first n natural numbers.

---
+++ Practice_Question35.py
@@ -0,0 +1,13 @@
+#Write a recursive function to calculate the sum of first n natural numbers.
+
+def cal_sum(n):
+
+    if(n==0):
+    
+        return 0
+    
+    return cal_sum(n-1) + n
+
+sum=cal_sum(5)
+
+print(sum)
