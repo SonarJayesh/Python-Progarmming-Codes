4052d35df136ffa1f91d490f7bae9033961f8a1a
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-16 23:20:46 +0530

    [ADDED]: Jayesh Python program for Create a new file Practice.tct using python. Add the following data in it:

---
+++ Practice_Question37.py
@@ -0,0 +1,8 @@
+#Create a new file "Practice.tct" using python. Add the following data in it:
+
+with open("practice.txt","w") as f:
+    
+    f.write("Hi everyone\n we are learning File I/O\n")
+    f.write("Using Java.\nI like Programming Java")
+
+f.close()
