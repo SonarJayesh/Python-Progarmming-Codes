122f68ae30807f82a859aa1f559b38a0103dd56f
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-22 00:59:59 +0530

    [ADDED]: Jayesh Python program for Class And Object Demo for create a simple Student Class And Call using that class object

---
+++ Class_Object_Demo.py
@@ -0,0 +1,11 @@
+class Student():
+    name = "Jayesh_Sonar"
+    age = 21
+    city = "Shahada"
+
+student = Student()
+student2 = Student()
+
+print(student.name)
+print(student2)
+print(student.age,"\n",student.city)
