3b4f2da525a278b1649a155af9decdae30cf3de0
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-21 22:29:16 +0530

    [ADDED]: Jayesh Python program for Abstraction code using constructor in python

---
+++ Abstraction_Demo.py
@@ -0,0 +1,15 @@
+#Abstraction Demo Code
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.cluch = False
+
+    def start(self):
+        self.cluch = True
+        self.acc = True    
+
+        print("Car Started....")
+
+car = Car()
+car.start()        
