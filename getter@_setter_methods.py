3a8af86a5701fa04eabac8064183d6721c797ff3
SonarJayesh <jayeshsonar2824@gmail.com>
2025-12-27 00:10:11 +0530

    [ADDED]: Jayesh Python progarm for Getter and Setter Method In python

---
+++ getter@_setter_methods.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self,name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name    
+    
+    @name.setter
+    def name(self,value):
+        self._name = value
+
+
+student = Student("Jayesh")
+print(student.name)
+
+student.name = "Sonar"
+print(student.name)
+
