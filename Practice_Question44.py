b5588ec2e489275ddbfb34404ac06e9a039afeab
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-05 21:51:01 +0530

    [ADDED]: Jayesh Python code for Define a Circle class to create a circle with radius r using the constructor.Define an Area() method of the class which calculates the area of the circle Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle

---
+++ Practice_Question44.py
@@ -0,0 +1,21 @@
+"""
+Qs. Define a Circle class to create a circle with radius r using the constructor.
+    Define an Area() method of the class which calculates the area of the circle.
+    Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle.
+"""
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2          
+
+    def perimeter(self):
+        return 2 * (22/7) *self.radius
+
+
+
+circle = Circle(21)
+print(circle.area())
+print(circle.perimeter())            
