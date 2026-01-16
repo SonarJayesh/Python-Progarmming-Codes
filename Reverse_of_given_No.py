07c30be70593135f069c74fb64f945d3771b3d23
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-17 00:11:41 +0530

    [ADDED]: Jayesh Python code for resvers the number using while loop

---
+++ Reverse_of_given_No.py
@@ -0,0 +1,9 @@
+#To find out the Reverse of the given Number.
+n = int(input("Enter a Number:"))
+rev = 0
+while(n>0):
+    rem = n%10
+    rev=rev*10+rem
+    n = int(n/10)
+
+print("Reverse of number: ",rev)    
