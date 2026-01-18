b37cbfd7fb6ad78425f166bbd6aebbe5d8c397cb
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-18 12:40:30 +0530

    [ADDED]: Jayesh Python code for Create a custom module named calculator with functions for addition, subtraction, multiplication, and division. Import it and use its functions in another program.

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# Create a custom module named calculator with functions for addition, subtraction, 
+# multiplication, and division. Import it and use its functions in another program.
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
