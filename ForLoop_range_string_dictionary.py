dc45b2978d70f2456b13319cfa0340c8c63371fb
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-24 22:24:45 +0530

    [ADDED]: Jayesh Python code for Demonstrate the use of a for loop with ranges, strings, and dictionaries.

---
+++ ForLoop_range_string_dictionary.py
@@ -0,0 +1,17 @@
+#Demonstrate the use of a for loop with ranges, strings, and dictionaries.
+
+#Using range
+
+for i in range(1,4):
+    print(i)
+
+#using String
+for ch in "python":
+    print(ch)
+
+#using Dictionary
+data = {"a":1, "b":2}
+
+for key,value in data.items():
+    print(key,value)
+    
