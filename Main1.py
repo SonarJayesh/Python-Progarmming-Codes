174146384595fca4821f67e4a2731e0d12087b6e
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-30 10:01:11 +0530

    [ADDED]: Jayesh Python Program for Create a module called math_utils with functions for addition, subtraction, multiplication, and division. Import and use it in another script.

---
+++ Main1.py
@@ -0,0 +1,9 @@
+#Create a module called math_utils with functions for addition, subtraction, 
+#multiplication, and division. Import and use it in another script.
+
+import math_utils
+
+print(math_utils.add(10,20))
+print(math_utils.sub(10,20))
+print(math_utils.mul(10,20))
+print(math_utils.div(10,20))
