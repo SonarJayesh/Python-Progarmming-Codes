1c694e0d87e0a45cefddc34d9a1a85eeff80a7e6
SonarJayesh <jayeshsonar2824@gmail.com>
2026-01-31 17:39:48 +0530

    [ADDED]: Jayesh Python Program for a module called math_utils with functions for addition, subtraction, multiplication, and division. Import and use it in another script.

---
+++ math_utils.py
@@ -0,0 +1,14 @@
+#Create a module called math_utils with functions for addition, subtraction, 
+# multiplication, and division. Import and use it in another script.
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
