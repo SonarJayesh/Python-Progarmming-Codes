cc6f9b0f5a06f0e5db12c03e02cedf97901f1602
SonarJayesh <jayeshsonar2824@gmail.com>
2026-02-02 18:41:44 +0530

    [ADDED]: Jayesh Python Program for calculate the factorial of a number using recursion.

---
+++ Recurtion_Factorial.py
@@ -0,0 +1,10 @@
+#Write a program to calculate the factorial of a number using recursion.
+
+def factorial(n):
+    if n ==0 or n ==1:
+        return 1
+    else:
+        return n* factorial(n-1)
+    
+num  = int (input("Enter The Number:"))
+print("Factorial: ", factorial(num))
